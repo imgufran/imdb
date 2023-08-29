3b28d4f4270c275feab4afbca6f95e7040dda12e
Gufran <iamgufran@outlook.com>
2023-08-29 10:25:56 +0530

    added the API to fetch movie data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h1 className="text-red-400">This text is coming from `Home` Component which is in `page.jsx` file</h1>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+// async-await -> used only in server Component.
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    {
+      next: {
+        // 10000 -> seconds
+        revalidate: 10000,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+
+  return (
+    <div>
+      <Results results={data.results}/>
+    </div>
+  );
 }
